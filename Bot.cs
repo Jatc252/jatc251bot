89da14c18697729df7bad1727300e202706ed860
Jatc252 <42489571+Jatc252@users.noreply.github.com>
2021-04-07 11:23:32 +1000

    Add project files.

---
+++ Bot.cs
@@ -0,0 +1,42 @@
+﻿using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jatc251Bot
+{
+    class Bot
+    {
+        public DiscordClient Client { get; private set; }
+        public CommandsNextExtension Commands { get; private set; }
+        public async Task RunAsync()
+        {
+            var config = new DiscordConfiguration
+            {
+
+            };
+
+            Client = new DiscordClient(config);
+
+            Client.Ready += OnClientReady;
+
+            var commandsConfig = new CommandsNextConfiguration
+            {
+
+            };
+
+            Commands = Client.UseCommandsNext(commandsConfig);
+
+            await Client.ConnectAsync();
+
+            await Task.Delay(1);
+        }
+        private Task OnClientReady(DiscordClient client, ReadyEventArgs e)
+        {
+            return null;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Jatc251Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bot = new Bot();
+        }
+    }
+}
